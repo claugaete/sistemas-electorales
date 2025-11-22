adb887ef0e5c8487ab3b03d44d2454aa10eac0c6
Claudio Gaete <claudio.gaete@ug.uchile.cl>
2025-11-22 16:07:13 -0300

    Corregir carga de datos de elecciones 2025

--- elecciones_2025/download_data.py
+++ elecciones_2025/download_data.py
@@ -1,5 +1,6 @@
 import asyncio
 from playwright.async_api import async_playwright
+import os
 
 async def download_data():
     async with async_playwright() as p:
@@ -11,15 +12,24 @@ async def download_data():
         await page.get_by_role("button", name="Diputados").click()
         await page.click("text=División Electoral Chile")
         
+        await asyncio.sleep(5)
+        
         for distrito in range(1, 29):
             select = page.locator("select.combo.text-filtros").nth(2)
             await select.select_option(label=f"DISTRITO {distrito}")
-            
+
+            await asyncio.sleep(3)
+
             async with page.expect_download() as download_info:
                 await page.locator('a[title="Descargar en EXCEL"]:visible').click()
+
+            await asyncio.sleep(3)
             
             download = await download_info.value
-            await download.save_as(f"datos_2025/distrito_{distrito}.xlsx")
+            await download.save_as(
+                os.path.dirname(os.path.abspath(__file__))
+                + f"/datos_2025/distrito_{distrito}.xlsx"
+            )
 
 if __name__ == "__main__":
     asyncio.run(download_data())
